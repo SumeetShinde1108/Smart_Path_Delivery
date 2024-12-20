8adaeb220c2234f35079da1bc7ceaca946a8f346
Sumeet Shinde <sumeetshinde118@gmail.com>
2024-12-20 14:27:21 +0530

    Modifications

--- Delivery/settings.py
+++ Delivery/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Delivery.urls'
+ROOT_URLCONF = 'delivery.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Delivery.wsgi.application'
+WSGI_APPLICATION = 'delivery.wsgi.application'
 
 
 # Database
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Delivery.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'delivery.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
