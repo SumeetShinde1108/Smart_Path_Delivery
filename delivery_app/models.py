d9781dcdd9875b2cda07a47ddf8642d3d13b6712
Sumeet Shinde <sumeetshinde118@gmail.com>
2024-12-20 10:59:37 +0530

    Add vehicle model

--- delivery_app/models.py
+++ delivery_app/models.py
@@ -1,3 +1,16 @@
+from datetime import date
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=64)
+    max_weight_capacity = models.FloatField()
+
+    class Meta:
+        verbose_name = "Vehicle"
+        verbose_name_plural = "Vehicles"
+
+    def __str__(self):
+        return self.name
+
+
