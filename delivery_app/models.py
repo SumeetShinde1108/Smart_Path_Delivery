584dfd0b6c5a379479fc9de893ba961bd36ffcb9
Sumeet Shinde <sumeetshinde118@gmail.com>
2024-12-26 20:56:45 +0530

    Modifications

--- delivery_app/models.py
+++ delivery_app/models.py
@@ -1,136 +1,137 @@
-from django.db import models
-from delivery_app.utils import calculate_distance 
+from django.contrib.gis.db import models
 
 
 class Location(models.Model):
-    address = models.CharField(max_length=64)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-
+    '''
+    Represents a location with an address and geographical coordinates.
+    '''
+    address = models.CharField(max_length=255)
+    point = models.PointField()
+    
     class Meta:
         verbose_name = "Location"
-        verbose_name_plural = "Locations"
+        verbose_name_plural = "01 Locations"
 
     def __str__(self):
-        return self.address
+        return f'Address :{self.address}'
 
 
 class Order(models.Model):
+    '''
+    Stores customer order details, including weight, location, and date.
+    '''
     order_id = models.CharField(max_length=64, unique=True)
     weight = models.FloatField(help_text="Weight in kilograms")
     delivery_location = models.ForeignKey(
         Location,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="Location_Order"
     )
     date_of_order = models.DateField()
 
     class Meta:
         verbose_name = "Order"
-        verbose_name_plural = "Orders"
+        verbose_name_plural = "02 Orders"
+        ordering = ['-date_of_order'] 
 
     def __str__(self):
         return f"Order {self.order_id} - {self.weight} kg"
 
 
 class Distance(models.Model):
+    '''
+    Tracks the distance between two locations. 
+    '''
     location_a = models.ForeignKey(
         Location,
-        on_delete=models.CASCADE, 
+        on_delete=models.CASCADE,
         related_name="distance_from"
     )
     location_b = models.ForeignKey(
-        Location, 
-        on_delete=models.CASCADE, 
+        Location,
+        on_delete=models.CASCADE,
         related_name="distance_to"
     )
-    distance = models.FloatField(null=True, blank=True)
+    distance = models.FloatField(help_text="Distance between two locations")
 
     class Meta:
-        unique_together = ("location_a", "location_b")
         verbose_name = "Distance"
-        verbose_name_plural = "Distances"
+        verbose_name_plural = "03 Distances"
+        unique_together = ("location_a", "location_b")
         
     def __str__(self):
         return f"{self.location_a.address} to {self.location_b.address}: {self.distance} km"
 
-    def save(self, *args, **kwargs):
-        if self.location_a == self.location_b:
-            raise ValueError("Locations A and B cannot be the same.")
-
-        if self.distance is None:
-            self.distance = calculate_distance(
-                self.location_a.latitude,
-                self.location_a.longitude,
-                self.location_b.latitude,
-                self.location_b.longitude,
-         )   
-
-        super().save(*args, **kwargs)
-
-    @classmethod
-    def get_distance(cls, loc_a, loc_b):
-        try:
-            return cls.objects.get(location_a=loc_a, location_b=loc_b).distance
-        except cls.DoesNotExist:
-            try:
-                return cls.objects.get(location_a=loc_b, location_b=loc_a).distance
-            except cls.DoesNotExist:
-                raise ValueError("Distance between locations not defined.")
-
 
 class Vehicle(models.Model):
+    '''
+    Represents a vehicle with its capacity, speed, and availability.
+    '''
     vehicle_no = models.CharField(max_length=64, unique=True)
     vehicle_capacity = models.FloatField(help_text="Weight capacity in kilograms")
     average_speed = models.FloatField(help_text="Average speed in km/h")
-    is_available = models.BooleanField(default=True)
+    is_available = models.BooleanField(
+        default=True, 
+        help_text='Avaibility of a vehicle',
+    )
 
     class Meta:
         verbose_name = "Vehicle"
-        verbose_name_plural = "Vehicles"
+        verbose_name_plural = "04 Vehicles"
+        ordering = ['average_speed']
 
     def __str__(self):
         return f"Vehicle {self.vehicle_no} - Capacity: {self.vehicle_capacity} kg"
 
 
 class Route(models.Model):
+    '''
+    Represent detail optimized routes for delivering orders using vehicles.
+    '''
     vehicles = models.ManyToManyField(Vehicle, blank=True)
     order = models.ForeignKey(
         Order, 
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="Order_Route"
     )
     total_distance = models.FloatField(help_text="Total distance covered by a single route in kms")
     total_weight = models.FloatField(
+        default=0,
         help_text="Total weight of all orders assigned to this route in kgs",
-        default=0
     )
     path = models.JSONField(default=list)
 
     class Meta:
         verbose_name = "Route"
-        verbose_name_plural = "Routes"
+        verbose_name_plural = "05 Routes"
+        ordering = ['-total_distance']
 
     def __str__(self):
         return f"Route {self.id} - {self.total_distance} km"
 
 
 class Delivery(models.Model):
+    '''
+    Plans and tracks deliveries for a specific date.
+    '''
     route = models.ForeignKey(
         Route, 
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="Route_Delivery"
     )
     total_vehicles = models.IntegerField(help_text="No of vehicles allocated for this delivery")
     total_distance = models.FloatField(help_text="Total distance covered by all routes in kms")
     total_weight = models.FloatField(
         help_text="Combined weight of all orders assigned to this delivery, in kilograms",
-        default=0
     )
     date_of_delivery = models.DateField()
-
+    
     class Meta:
-        verbose_name = "Delivery Planning"
-        verbose_name_plural = "Delivery Plannings"
+        verbose_name = "Delivery"
+        verbose_name_plural = "06 Deliveries"
+        ordering = ['-date_of_delivery']
 
     def __str__(self):
-        return f"Delivery Plan {self.date_of_delivery} - {self.total_vehicles} vehicles"
+        return f"Delivery date {self.date_of_delivery} - {self.total_vehicles} vehicles"
 
     
