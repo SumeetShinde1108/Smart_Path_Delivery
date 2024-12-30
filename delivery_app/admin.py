a6a29fa8a96fe111cc93900f0153dfeeaf214232
Sumeet Shinde <sumeetshinde118@gmail.com>
2024-12-30 11:36:46 +0530

    Modifications

--- delivery_app/admin.py
+++ delivery_app/admin.py
@@ -19,7 +19,7 @@ class StoreInline(admin.TabularInline):
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('address', 'point')
+    list_display = ('address', 'point',)
     search_fields = ('address',)
     ordering = ('address',)
     Inlines = [OrderInline, StoreInline]
@@ -27,22 +27,22 @@ class LocationAdmin(admin.ModelAdmin):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('order_id', 'weight', 'delivery_location', 'date_of_order')
+    list_display = ('order_id', 'weight', 'delivery_location', 'date_of_order',)
     list_filter = ('date_of_order',)
-    search_fields = ('order_id', 'delivery_location__address')
+    search_fields = ('order_id', 'delivery_location__address',)
     ordering = ('-date_of_order',)
 
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ('name', 'location')
-    search_fields = ('name', 'location__address')
+    list_display = ('name', 'location',)
+    search_fields = ('name', 'location__address',)
     ordering = ('name',)
 
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ('vehicle_no', 'capacity', 'average_speed')
+    list_display = ('vehicle_no', 'capacity', 'average_speed',)
     list_filter = ('average_speed',)
     search_fields = ('vehicle_no',)
     ordering = ('average_speed',)
@@ -50,13 +50,9 @@ class VehicleAdmin(admin.ModelAdmin):
 
 @admin.register(Delivery)
 class DeliveryAdmin(admin.ModelAdmin):
-    list_display = ('store', 'date_of_delivery', 'total_weight', 'vehicle')
+    list_display = ('store', 'date_of_delivery', 'total_weight',)
     list_filter = ('date_of_delivery',)
-    search_fields = ('store__name', 'store__location__address')
+    search_fields = ('store__name', 'store__location__address',)
     ordering = ('-date_of_delivery',)
 
-
-    def vehicle(self, obj):
-        return obj.vehicles.all()
-    vehicle.short_description = 'Vehicles'
 
