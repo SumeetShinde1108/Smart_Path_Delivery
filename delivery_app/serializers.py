cae73c984c33a7e4e8a6f8d8d8ae217dd601f9e4
Sumeet Shinde <sumeetshinde118@gmail.com>
2025-01-16 10:54:11 +0530

    Add Order Serializer

--- delivery_app/serializers.py
+++ delivery_app/serializers.py
@@ -6,3 +6,26 @@ class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = ["id", "address", "point"]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    delivery_location = LocationSerializer()
+
+    class Meta:
+        model = Order
+        fields = ["id", "order_id", "weight", "delivery_location", "date_of_order"]
+
+    def create(self, validated_data):
+        location_data = validated_data.pop("delivery_location")
+        location = Location.objects.create(**location_data)
+        order = Order.objects.create(delivery_location=location, **validated_data)
+        return order
+
+    def update(self, instance, validated_data):
+        location_data = validated_data.pop("delivery_location", None)
+        if location_data:
+            for attr, value in location_data.items():
+                setattr(instance.delivery_location, attr, value)
+            instance.delivery_location.save()
+        return super().update(instance, validated_data)
+
